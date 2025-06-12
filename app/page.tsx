708fca0c4bde9064371824d0f8d91b7ac2524feb
larissasn <s4nlari@gmail.com>
2025-06-12 17:23:39 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
-import { Button } from "./_components/ui/button"
+import Header from "@/components/header"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function page() {
-  return <Button>Teste</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Larissa!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores da  Barber House"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
