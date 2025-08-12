150b0ad2d49890f177478cf5fcddf21cf23cfe81
larissasn <s4nlari@gmail.com>
2025-08-12 19:48:52 -0300

    feat: add  initial barbershop page

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
 import { Badge } from "./ui/badge"
 import { StarIcon } from "lucide-react"
+import Link from "next/link"
 
 interface BarberShopItemItems {
   barbershop: Barbershop
@@ -33,8 +34,8 @@ export default function BarberShopItem({ barbershop }: BarberShopItemItems) {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
