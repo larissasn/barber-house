daae95a0b5371bec458b139145cee90390278b50
larissasn <s4nlari@gmail.com>
2025-08-30 18:20:43 -0300

    feat: ensure user can search for barbershop

---
+++ app/barbershops/page.tsx
@@ -0,0 +1,43 @@
+import { Search } from "lucide-react"
+import BarberShopItem from "../_components/barbershop-item"
+import Header from "../_components/header"
+import { db } from "../_lib/prisma"
+
+interface BarberShopsPageProps {
+  searchParams: {
+    search?: string
+  }
+}
+
+const BarberShopsPage = async ({ searchParams }: BarberShopsPageProps) => {
+  const barbershops = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams?.search,
+        mode: "insensitive",
+      },
+    },
+  })
+
+  return (
+    <div>
+      <Header />
+      <div className="my-6 p-5">
+        <Search />
+      </div>
+
+      <div className="p-5">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Resultados para {searchParams.search}
+        </h2>
+        <div className="grid grid-cols-2 gap-4">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarberShopsPage
